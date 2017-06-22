9b18c0654aabbd98621910679e53467ad39e8aa8
20161104577 <y20161104577@mac04.local>
2017-06-22 08:51:26 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by 帅气的杨同学 on 2017/6/22.
+//  Copyright © 2017年 帅气的杨同学. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
