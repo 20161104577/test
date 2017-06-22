fcd477630a56f78593a43d109aadd4e26854120f
20161104577 <y20161104577@mac04.local>
2017-06-22 09:58:07 +0800

    软件工程大作业
    第一次上传

--- test/main.c
+++ test/main.c
@@ -7,9 +7,35 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void load()
+{
+    char str1[100];//$GPRMC第一行
+    char str2[100];//$GPGGA第二行
+    int time[10];
+    char lat[10];//纬度；
+    char latitude;//纬度方向；
+    char lon[10];//经度；
+    char longitude;//经度方向；
+    FILE *fp;
+    int i;
+    fp = fopen("//Users//y20161104577//Desktop//test//GPS.log","r");
+    fscanf(fp,"%s %s",&str1,&str2);
+    for(i=0; i<8; i++) {
+        lat[i] = str1[i+16];
+        lon[i] = lat[i];
+        if(i<6)
+        time[i] = (int)str1[i+8];
+    }
+    printf("%s\n%s\n%d\n",str1,str2);
+    for(i=0; i<6; i++) {
+        printf("%d",time[i]);
+    }
+        
+    fclose(fp);
+}
+int main() {
+    load();
     return 0;
 }
